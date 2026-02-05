b6ebaa697db47b8b44e5642a561bff28a59c9574
abhishek.kale <abhisahebpatil@gmail.com>
2026-02-05 10:50:14 +0530

    completed resolver guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TableDashboardPage } from './component/table-dashboard-page/table-dashboard-page';
+import { ProductResolver } from './guards/product.resolver';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '', pathMatch: 'full' },
+    { path: 'producttable', component: TableDashboardPage, resolve: { productDetails: ProductResolver } }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('basicResolverGuards');
+
+  constructor(private router: Router) { }
+
+  navigateProductsDetailsList() {
+    this.router.navigate(['producttable']);
+  }
 }
