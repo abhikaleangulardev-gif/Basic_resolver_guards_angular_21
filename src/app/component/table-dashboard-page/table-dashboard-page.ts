a24fb44b7ad810a8f9e554c0a91e3b8661abb763
abhishek.kale <abhisahebpatil@gmail.com>
2026-02-05 11:09:20 +0530

    completed pagination code

--- src/app/component/table-dashboard-page/table-dashboard-page.ts
+++ src/app/component/table-dashboard-page/table-dashboard-page.ts
@@ -8,13 +8,30 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './table-dashboard-page.html',
   styleUrl: './table-dashboard-page.css',
 })
-export class TableDashboardPage implements OnInit{
-  myProduct:any[] = [];
-  constructor(private ActivedRoute:ActivatedRoute){}
+export class TableDashboardPage implements OnInit {
+  myProduct: any[] = [];
+
+  // create a pagination
+  myPageSize: number = 5;
+  currentPage: number = 1;
+  constructor(private ActivedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     const myProductDetails = this.ActivedRoute.snapshot.data['productDetails'].products;
     console.log(myProductDetails);
     this.myProduct = myProductDetails;
+  }
+
+  get paginationProduct() {
+    const start = (this.currentPage - 1) * this.myPageSize;
+    return this.myProduct.slice(start, start + this.myPageSize);
+  }
+
+  get totalPage(){
+    return Math.ceil(this.myProduct.length/this.myPageSize);
+  }
+
+  changePage(page: number) {
+    this.currentPage = page;
   }
 }
